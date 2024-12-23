44f81e8876f39dce8d3cfd63101ec7be0f6f5186
Stephen882-pixel <stephenondeyo0@gmail.com>
2024-12-23 11:38:55 +0300

    createBook

--- main.go
+++ main.go
@@ -26,10 +26,18 @@ func getBooks(c *gin.Context){
 
 func createBook(c *gin.Context){
 	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil{
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated,newBook)
 }
 
 func main(){
 	router := gin.Default()
 	router.GET("/books",getBooks)
+	router.POST("books",createBook)
 	router.Run("localhost:8081")
 }
